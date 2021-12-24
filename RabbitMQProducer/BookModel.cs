6cc287e68033594985ecce4595f9322e42e0abee
IndienkovaAnastasia <43013482+indienkova@users.noreply.github.com>
2021-12-24 19:12:42 +0300

    Add Producer

---
+++ RabbitMQProducer/BookModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQProducer
+{
+    public class BookModel
+    {
+        public string Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public int PagesCount { get; set; }
+    }
+}
